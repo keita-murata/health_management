03afd284f465c89561ba3c07b9507874dd91b72e
keita.murata <68623062+keita-murata@users.noreply.github.com>
2020-08-07 16:19:47 +0900

    トップページ作成　リンク発行　Rspecの初期設定

---
+++ app/controllers/top_pages_controller.rb
@@ -0,0 +1,10 @@
+class TopPagesController < ApplicationController
+    def home
+    end
+
+    def help
+    end
+
+    def message
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "top_pages#home"
+  get  "top_pages/help"
+  get  "top_pages/message"
 end
