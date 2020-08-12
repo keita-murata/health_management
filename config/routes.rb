067285c2662316b6832d69dd3751793bd4ad167d
keita.murata <68623062+keita-murata@users.noreply.github.com>
2020-08-12 18:57:18 +0900

    devise設定　Viewの確認まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root "top_pages#home"
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  # devise_scope :user do
+  #   get "sign_in", :to => "users/sessions#new"
+  #   get "sign_out", :to => "users/sessions#destroy" 
+  # end
+  root to: "top_pages#home"
   get  "top_pages/help"
   get  "top_pages/message"
 end
