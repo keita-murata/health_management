bee0f606deba1601b20724d47700a16801514e02
keita.murata <68623062+keita-murata@users.noreply.github.com>
2020-08-26 20:36:29 +0900

    ユーザー情報の編集が更新されていない状態

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,9 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   } 
-
-  # devise_scope :user do
-  #   get "sign_in", :to => "users/sessions#new"
-  #   get "sign_out", :to => "users/sessions#destroy" 
-  # end
   root to: "top_pages#home"
   get  "top_pages/help"
   get  "top_pages/message"
+
+  resources :users
 end
