0a8c878c7ec9cfb7ebc7e42b294043e565da7bc1
keita.murata <68623062+keita-murata@users.noreply.github.com>
2020-10-06 14:54:02 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :trainings, dependent: :destroy
   has_many :events, dependent: :destroy
   validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, unipueness: true
+  validates :email, length: { maximum: 50 }
+  validates :email, length: { minimum: 8 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
